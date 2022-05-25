519ad12805f0ef434dead62dc1193e8ddbd8ae6b
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-05-26 01:19:33 +0900

    [feat] Error 처리 기능 구현

---
+++ backend/src/main/java/com/hoppy/app/response/error/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.hoppy.app.response.error.exception;
+
+public class EntityNotFoundException extends BusinessException {
+
+    public EntityNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
