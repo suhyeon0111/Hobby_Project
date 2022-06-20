b72e520c40e779e6578c4c9f853bebd67ce0ad2a
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-06-21 01:33:51 +0900

    [feat] 멤버, 모임 중간 테이블 생성

---
+++ backend/src/main/java/com/hoppy/app/member/domain/MemberMeeting.java
@@ -0,0 +1,35 @@
+package com.hoppy.app.member.domain;
+
+import com.hoppy.app.meeting.domain.Meeting;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberMeeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+}
