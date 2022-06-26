5e409d0b97faf76b5a0d391f21bcb86590ac0491
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-06-26 15:28:21 +0900

    [feat] api docs update

--- backend/src/test/java/com/hoppy/app/upload/controller/UploadControllerTest.java
+++ backend/src/test/java/com/hoppy/app/upload/controller/UploadControllerTest.java
@@ -1,4 +1,13 @@
-package com.hoppy.app.health.controller;
+package com.hoppy.app.upload.controller;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.hoppy.app.EnableMockMvc;
 import org.junit.jupiter.api.Test;
@@ -12,32 +21,24 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.hamcrest.core.Is.is;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @ExtendWith(SpringExtension.class)
 @EnableMockMvc
 @AutoConfigureRestDocs
 @ActiveProfiles("dev1")
-class HealthControllerTest {
+class UploadControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void checkHealth() throws Exception {
+    void getPresignedUrl() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders
-                .get("/health"))
+                        .get("/upload/presigned/test"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("message", is("정상 동작 중")))
-                .andDo(document("health-check",
+                .andExpect(jsonPath("message", is("URL 발급 완료")))
+                .andDo(document("get-presigned-url",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint())
                 ));
