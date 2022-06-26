9254fe29e7192e957f7df55f570a725830ef25e6
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-06-26 15:57:30 +0900

    [feat] Meeting Manage 서비스 정의

---
+++ backend/src/main/java/com/hoppy/app/meeting/service/MeetingManageServiceImpl.java
@@ -0,0 +1,66 @@
+package com.hoppy.app.meeting.service;
+
+import com.hoppy.app.meeting.common.Category;
+import com.hoppy.app.meeting.domain.Meeting;
+import com.hoppy.app.meeting.dto.CreateMeetingDto;
+import com.hoppy.app.meeting.repository.MeetingRepository;
+import com.hoppy.app.member.domain.Member;
+import com.hoppy.app.member.domain.MemberMeeting;
+import com.hoppy.app.member.repository.MemberMeetingRepository;
+import com.hoppy.app.response.dto.ResponseDto;
+import com.hoppy.app.response.error.exception.BusinessException;
+import com.hoppy.app.response.error.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MeetingManageServiceImpl implements MeetingManageService {
+
+    private final MeetingRepository meetingRepository;
+    private final MemberMeetingRepository memberMeetingRepository;
+
+    @Override
+    public void saveMeeting(Meeting meeting) {
+        meetingRepository.save(meeting);
+    }
+
+    @Override
+    public void createAndSaveMemberMeetingData(Meeting meeting, Member member) {
+
+        MemberMeeting memberMeeting = MemberMeeting.builder()
+                .meeting(meeting)
+                .member(member)
+                .build();
+
+        memberMeetingRepository.save(memberMeeting);
+    }
+
+    @Override
+    public Meeting createMeeting(CreateMeetingDto dto) throws BusinessException {
+        // 이름 중복 검사
+        if(checkTitleDuplicate(dto.getTitle())) {
+            throw new BusinessException(ErrorCode.TITLE_DUPLICATE);
+        }
+
+        // 카테고리 유효성 검사
+        Category category = Category.intToEnum(dto.getCategory());
+        if(category == Category.ERROR) {
+            throw new BusinessException(ErrorCode.CATEGORY_ERROR);
+        }
+
+        return Meeting.builder()
+                .category(category)
+                .url("https://hoppyservice.s3.ap-northeast-2.amazonaws.com/" + dto.getFilename())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .memberLimit(dto.getMemberLimit())
+                .build();
+    }
+
+    @Override
+    public boolean checkTitleDuplicate(String title) {
+        return 0 < meetingRepository.countMeetingByTitle(title);
+    }
+}
