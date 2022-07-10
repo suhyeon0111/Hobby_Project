bd0308068591f094f0070df0094280fbabc2e009
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-07-10 21:20:11 +0900

    [feat] 페이징 api 구현

--- backend/src/main/java/com/hoppy/app/meeting/controller/MeetingController.java
+++ backend/src/main/java/com/hoppy/app/meeting/controller/MeetingController.java
@@ -1,20 +1,30 @@
 package com.hoppy.app.meeting.controller;
 
+import com.hoppy.app.meeting.Category;
 import com.hoppy.app.meeting.domain.Meeting;
 import com.hoppy.app.meeting.dto.CreateMeetingDto;
+import com.hoppy.app.meeting.dto.MeetingDto;
+import com.hoppy.app.meeting.dto.PagingMeetingDto;
 import com.hoppy.app.meeting.service.MeetingInquiryService;
 import com.hoppy.app.meeting.service.MeetingManageService;
 import com.hoppy.app.member.domain.Member;
 import com.hoppy.app.member.service.MemberService;
 import com.hoppy.app.response.dto.ResponseDto;
+import com.hoppy.app.response.error.exception.BusinessException;
+import com.hoppy.app.response.error.exception.ErrorCode;
 import com.hoppy.app.response.service.ResponseService;
 import com.hoppy.app.response.service.SuccessCode;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,8 +39,8 @@ public class MeetingController {
 
     @PostMapping
     public ResponseEntity<ResponseDto> createMeeting(@RequestBody @Valid CreateMeetingDto dto) {
-        Meeting meeting = meetingManageService.createMeeting(dto);
         Member member = memberService.findMemberById(dto.getMemberId());
+        Meeting meeting = meetingManageService.createMeeting(dto);
 
         /*
         * 아래에 있는 두개의 save 메서드 호출 순서를 서로 바꾸면
@@ -41,5 +51,29 @@ public class MeetingController {
         meetingManageService.createAndSaveMemberMeetingData(meeting, member);
 
         return responseService.successResult(SuccessCode.CREATE_MEETING_SUCCESS);
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseDto> getMeetingListByCategoryWithPaging(
+            @RequestParam int categoryNumber,
+            @RequestParam long lastId
+    ) {
+        Category category = Category.intToCategory(categoryNumber);
+        if(category == Category.ERROR) {
+            throw new BusinessException(ErrorCode.CATEGORY_ERROR);
+        }
+
+        Long memberId = 1L; // 유저 아이디
+
+        List<Meeting> meetingList = meetingInquiryService.listMeetingByCategory(category, lastId);
+        lastId = meetingInquiryService.getListsLastMeetingId(meetingList);
+        String nextPagingUrl = meetingInquiryService.createNextPagingUrl(categoryNumber, lastId);
+        List<MeetingDto> meetingDtoList = meetingInquiryService.meetingListToMeetingDtoList(meetingList, memberId);
+        PagingMeetingDto pagingMeetingDto = PagingMeetingDto.builder()
+                .meetingDtoList(meetingDtoList)
+                .nextPagingUrl(nextPagingUrl)
+                .build();
+
+        return responseService.successResult(SuccessCode.INQUIRY_MEETING_SUCCESS, pagingMeetingDto);
     }
 }
