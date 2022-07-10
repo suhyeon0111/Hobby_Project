9e5f63a6697188c89c370fbcfe71fa5facdcfc99
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-07-10 21:16:55 +0900

    [feat] 페이징 서비스 구현

--- backend/src/main/java/com/hoppy/app/meeting/service/MeetingInquiryService.java
+++ backend/src/main/java/com/hoppy/app/meeting/service/MeetingInquiryService.java
@@ -1,6 +1,9 @@
 package com.hoppy.app.meeting.service;
 
 import com.hoppy.app.meeting.Category;
+import com.hoppy.app.meeting.domain.Meeting;
+import com.hoppy.app.meeting.dto.MeetingDto;
+import java.util.List;
 
 /*
 * 모임 조회 서비스
@@ -10,5 +13,11 @@ import com.hoppy.app.meeting.Category;
 * */
 public interface MeetingInquiryService {
 
-    public void listMeetingByCategory(Category category);
+    public List<Meeting> listMeetingByCategory(Category category, long lastId);
+
+    public long getListsLastMeetingId(List<Meeting> meetingList);
+
+    public String createNextPagingUrl(int categoryNumber, long lastId);
+
+    public List<MeetingDto> meetingListToMeetingDtoList(List<Meeting> meetingList, Long memberId);
 }
--- backend/src/main/java/com/hoppy/app/meeting/service/MeetingInquiryServiceImpl.java
+++ backend/src/main/java/com/hoppy/app/meeting/service/MeetingInquiryServiceImpl.java
@@ -2,9 +2,17 @@ package com.hoppy.app.meeting.service;
 
 import com.hoppy.app.meeting.Category;
 import com.hoppy.app.meeting.domain.Meeting;
+import com.hoppy.app.meeting.dto.MeetingDto;
 import com.hoppy.app.meeting.repository.MeetingRepository;
+import com.hoppy.app.member.domain.MemberMeetingLike;
+import com.hoppy.app.member.repository.MemberMeetingLikeRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +20,33 @@ import org.springframework.stereotype.Service;
 public class MeetingInquiryServiceImpl implements MeetingInquiryService {
 
     private final MeetingRepository meetingRepository;
+    private final MemberMeetingLikeRepository memberMeetingLikeRepository;
 
     @Override
-    public void listMeetingByCategory(Category category) {
+    public List<Meeting> listMeetingByCategory(Category category, long lastId) {
+        return meetingRepository.infiniteScrollPagingMeeting(category, lastId, PageRequest.of(0, 14));
+    }
 
-        List<Meeting> meetingList = meetingRepository.findAllMeetingByCategoryUsingFetch(category);
+    @Override
+    public long getListsLastMeetingId(List<Meeting> meetingList) {
+        return meetingList.get(meetingList.size() - 1).getId();
+    }
 
-        
+    @Override
+    public String createNextPagingUrl(int categoryNumber, long lastId) {
+        return "https://hoppy.kro.kr/api/meeting?categoryNumber=" + categoryNumber + "&lastId=" + lastId;
+    }
+
+    @Override
+    public List<MeetingDto> meetingListToMeetingDtoList(List<Meeting> meetingList, Long memberId) {
+        List<Long> likeIdList = memberMeetingLikeRepository
+                .findAllByMemberId(memberId)
+                .stream()
+                .map(MemberMeetingLike::getMeetingId)
+                .collect(Collectors.toList());
+
+        return meetingList.stream()
+                .map(M -> MeetingDto.meetingToMeetingDto(M, likeIdList.contains(M.getId())))
+                .collect(Collectors.toList());
     }
 }
