4015ebe38f0f5a7b70a61576eba58d32c1dfacdb
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-07-22 05:26:57 +0900

    [refactor] 멤버 페이징 서비스 추가

--- backend/src/main/java/com/hoppy/app/member/service/MemberService.java
+++ backend/src/main/java/com/hoppy/app/member/service/MemberService.java
@@ -2,6 +2,8 @@ package com.hoppy.app.member.service;
 
 import com.hoppy.app.member.domain.Member;
 import com.hoppy.app.member.dto.UpdateMemberDto;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
 
 public interface MemberService {
 
@@ -10,4 +12,6 @@ public interface MemberService {
     public Member updateMemberById(Long memberId, UpdateMemberDto memberDto);
 
     public Member deleteMemberById(Long id);
+
+    public List<Member> infiniteScrollPagingMember(List<Long> memberIdList, Long lastId, PageRequest pageRequest);
 }
