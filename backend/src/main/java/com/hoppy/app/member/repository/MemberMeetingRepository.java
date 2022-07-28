df0e27113ecdf87bb3d382cb634a3a535a72bf4c
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-07-29 03:35:58 +0900

    [feat] 탈퇴 쿼리 추가

--- backend/src/main/java/com/hoppy/app/member/repository/MemberMeetingRepository.java
+++ backend/src/main/java/com/hoppy/app/member/repository/MemberMeetingRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberMeetingRepository extends JpaRepository<MemberMeeting, Long> {
 
+    void deleteMemberMeetingByMeetingIdAndMemberId(Long meetingId, Long memberId);
 }
