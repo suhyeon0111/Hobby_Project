964a61163aa021a6551ee95c1959fa9c36675328
Daehan Choi <mnnm22@naver.com>
2022-08-06 16:53:43 +0900

    [test] 스토리 CRUD 기능 추가

--- backend/src/test/java/com/hoppy/app/story/controller/StoryControllerTest.java
+++ backend/src/test/java/com/hoppy/app/story/controller/StoryControllerTest.java
@@ -2,7 +2,12 @@ package com.hoppy.app.story.controller;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hoppy.app.login.WithMockCustomUser;
@@ -10,6 +15,7 @@ import com.hoppy.app.login.auth.SocialType;
 import com.hoppy.app.member.Role;
 import com.hoppy.app.member.domain.Member;
 import com.hoppy.app.member.repository.MemberRepository;
+import com.hoppy.app.story.domain.story.Story;
 import com.hoppy.app.story.dto.UploadStoryDto;
 import com.hoppy.app.story.repository.StoryRepository;
 import java.nio.charset.StandardCharsets;
@@ -19,6 +25,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -29,6 +36,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 class StoryControllerTest {
 
@@ -58,6 +66,12 @@ class StoryControllerTest {
                         .intro("잘부탁드립니다.")
                         .build()
         );
+        for(int i = 1; i <= 5; i++) {
+            storyRepository.save(
+                    Story.builder().memberId(8669L).title(i+"th Story").content("This is " + i + "th Story").filePath(i+".jpg")
+                            .username("CHOI").build()
+            );
+        }
     }
 
     @Test
@@ -66,15 +80,62 @@ class StoryControllerTest {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Long id = Long.parseLong(authentication.getName());
         Optional<Member> optMember = memberRepository.findById(id);
-        assertThat(optMember.isPresent());
-
-        UploadStoryDto dto = UploadStoryDto.builder().title("1th Story Test").content("1th Upload").filename("1.avi").build();
+        assertThat(optMember.isPresent()).isTrue();
+        UploadStoryDto dto = UploadStoryDto.builder().title("Story Upload Test").content("This is Test code").filename("example.jpg").build();
         String content = objectMapper.writeValueAsString(dto);
         ResultActions result = mvc.perform(MockMvcRequestBuilders
-                .post("/story")
+                .post("/story/upload")
                 .content(content)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
         ).andDo(print());
+        result.andExpect(status().isOk())
+                .andDo(document("upload-story",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    void updateStory() throws Exception {
+        String storyId = "3";
+        Optional<Story> optStory = storyRepository.findById(Long.parseLong(storyId));
+        assertThat(optStory.isPresent()).isTrue();
+        assertThat(optStory.get().isDeleted()).isFalse();
+        UploadStoryDto dto = UploadStoryDto.builder().title("Update Story Test").content("This is updated story").filename("test_success.wav").build();
+        String content = objectMapper.writeValueAsString(dto);
+        ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .post("/story/update")
+                .param("id", storyId)
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+        ).andDo(print());
+        result.andExpect(status().isOk())
+                .andDo(document("update-story",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    void deleteStory() throws Exception {
+        String storyId = "4";
+        Optional<Story> optStory = storyRepository.findById(Long.parseLong(storyId));
+        assertThat(optStory.isPresent()).isTrue();
+        assertThat(optStory.get().isDeleted()).isFalse();
+
+        ResultActions result = mvc.perform(MockMvcRequestBuilders
+                .get("/story/delete")
+                .param("id", storyId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+        ).andDo(print());
+
+        Story story = storyRepository.findById(Long.parseLong(storyId)).get();
+        assertThat(story.isDeleted()).isTrue();
+
+        result.andExpect(status().isOk())
+                .andDo(document("delete-story",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
     }
 }
