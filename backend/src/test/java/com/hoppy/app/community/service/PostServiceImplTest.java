a9b95ae453f380b15ba945e0e6cc094995a6b41f
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-08-07 13:53:59 +0900

    [feat] 게시물 페이징 메서드 테스트 코드 작성

--- backend/src/test/java/com/hoppy/app/community/service/PostServiceImplTest.java
+++ backend/src/test/java/com/hoppy/app/community/service/PostServiceImplTest.java
@@ -1,20 +1,31 @@
 package com.hoppy.app.community.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.hoppy.app.community.domain.Post;
 import com.hoppy.app.community.repository.PostRepository;
+import com.hoppy.app.like.domain.LikeManager;
+import com.hoppy.app.like.repository.LikeManagerRepository;
+import com.hoppy.app.meeting.Category;
+import com.hoppy.app.meeting.domain.Meeting;
+import com.hoppy.app.meeting.repository.MeetingRepository;
+import com.hoppy.app.member.domain.Member;
+import com.hoppy.app.member.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
 
 /**
  * @author 태경 2022-08-06
  */
 @DataJpaTest
-@TestInstance(Lifecycle.PER_CLASS)
 class PostServiceImplTest {
 
     // TODO: 2022.08.06. Post 서비스 테스트 코드 작성이 필요함
@@ -22,13 +33,54 @@ class PostServiceImplTest {
     @Autowired
     PostRepository postRepository;
 
-    @BeforeAll
-    void before() {
+    @Autowired
+    MemberRepository memberRepository;
 
+    @Autowired
+    MeetingRepository meetingRepository;
+
+    @AfterEach
+    void afterEach() {
+        postRepository.deleteAll();
+        memberRepository.deleteAll();
+        meetingRepository.deleteAll();
     }
 
     @Test
-    void listPostByMeetingWithPaging() {
+    void pagingPostListTest() {
+        // given
+        Member member = memberRepository.save(
+                Member.builder()
+                        .id(1L)
+                        .username("test-name")
+                        .build()
+        );
+        Meeting meeting = meetingRepository.save(
+                Meeting.builder()
+                        .ownerId(member.getId())
+                        .title("test-title")
+                        .content("test-content")
+                        .category(Category.LIFE)
+                        .memberLimit(10)
+                        .build()
+        );
+        final int POST_COUNT = 10;
+        for(int i = 0; i < POST_COUNT; i++) {
+            postRepository.save(
+                    Post.builder()
+                            .title((i + 1) + "-title")
+                            .content((i + 1) + "-content")
+                            .owner(member)
+                            .meeting(meeting)
+                            .build()
+            );
+        }
 
+        // when
+        final int PAGING_SIZE = 8;
+        var posts = postRepository.infiniteScrollPagingPost(meeting, Long.MAX_VALUE, PageRequest.of(0, PAGING_SIZE));
+
+        // then
+        assertThat(posts.size()).isEqualTo(PAGING_SIZE);
     }
 }
