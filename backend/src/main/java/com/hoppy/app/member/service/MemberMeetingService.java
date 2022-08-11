ba036f7214e239d49d6212801854f3d3e5ff512e
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-08-11 15:55:23 +0900

    [refactor] 불필요 코드 제거

--- backend/src/main/java/com/hoppy/app/member/service/MemberMeetingService.java
+++
@@ -1,8 +0,0 @@
-package com.hoppy.app.member.service;
-
-import com.hoppy.app.member.domain.Member;
-
-public interface MemberMeetingService {
-
-    public void joinToMeetingById(Long memberId, Long metingId);
-}
