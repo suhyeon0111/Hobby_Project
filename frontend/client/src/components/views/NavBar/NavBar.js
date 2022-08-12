163c4380ba84e3db32056bafbd6ca4e384778ea0
Yoona-J <yoonalove77@naver.com>
2022-08-12 12:29:01 +0900

    [feature] make story upload logic

--- frontend/client/src/components/views/NavBar/NavBar.js
+++ frontend/client/src/components/views/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import { Drawer, Button, Icon } from 'antd';
 import './Sections/Navbar.css';
 import LogoImg from './img/logo.png'
 import Bell from './img/Bell.png'
+// import { Link } from 'react-router-dom';
 
 function NavBar() {
   const [visible, setVisible] = useState(false);
@@ -16,6 +17,9 @@ function NavBar() {
   const onClose = () => {
     setVisible(false);
   };
+
+  // const token = localStorage.getItem('Authorization')
+  // console.log(token)
 
   return (
     <nav
@@ -47,7 +51,7 @@ function NavBar() {
           visible={visible}
         >
           <div style={{ marginTop: '100px' }}>
-            <img src={LogoImg} style={{ width: '107px', height: '54px', marginLeft: '25%', marginBottom: '30px'}} />
+            <img src={LogoImg} alt="로고이미지" style={{ width: '107px', height: '54px', marginLeft: '25%', marginBottom: '30px'}} />
             <RightMenu mode="inline" />
             <hr style={{ width: "80%", backgroundColor: "#D3BA9C" }} />
             <LeftMenu mode="inline" />
@@ -56,12 +60,12 @@ function NavBar() {
       </div>
       <div style={{ float: 'right' }}>
         <a href="/notification" className="menu__bell">
-          <img src={Bell} style={{ width: "45px" }} />
+          <img src={Bell} alt="알림" style={{ width: "45px" }} />
         </a>
       </div>
       <div className="menu__logo">
-        <a href="/">
-          <img src={LogoImg} style={{ width: "147px" }} />
+        <a href='/'>
+          <img src={LogoImg} alt="로고" style={{ width: "147px" }} />
         </a>
       </div>
     </nav>
