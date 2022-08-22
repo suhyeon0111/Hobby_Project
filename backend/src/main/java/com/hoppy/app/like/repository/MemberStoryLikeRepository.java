72e906d4474a744170814256bc3bb92c5610f9ab
Choidaehan <71701634+Choidaehankr@users.noreply.github.com>
2022-08-22 23:37:20 +0900

    [fix] like 패키지에 스토리 관계 추가

---
+++ backend/src/main/java/com/hoppy/app/like/repository/MemberStoryLikeRepository.java
@@ -0,0 +1,14 @@
+package com.hoppy.app.like.repository;
+
+import com.hoppy.app.like.domain.MemberStoryLike;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberStoryLikeRepository extends JpaRepository<MemberStoryLike, Long> {
+
+    @Query("select s from MemberStoryLike s where s.member.id = :memberId and s.story.id = :storyId")
+    Optional<MemberStoryLike> findByMemberIdAndStoryId(Long memberId, Long storyId);
+}
