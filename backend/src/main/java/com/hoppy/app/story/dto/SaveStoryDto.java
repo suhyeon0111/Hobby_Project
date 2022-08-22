163f4d943a1dfe374a273314e02f0d5060353a53
Choidaehan <71701634+Choidaehankr@users.noreply.github.com>
2022-08-22 23:36:07 +0900

    [fix] 스토리 Dto 네이밍 변경 및 추가

--- backend/src/main/java/com/hoppy/app/story/dto/SaveStoryDto.java
+++ backend/src/main/java/com/hoppy/app/story/dto/SaveStoryDto.java
@@ -1,51 +1,48 @@
 package com.hoppy.app.story.dto;
 
+import com.hoppy.app.member.domain.Member;
 import com.hoppy.app.story.domain.story.Story;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Setter
+/**
+ * 스토리 업로드, 업데이트 시 단순히 스토리 정보를 반환하기 위한 Dto
+ */
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
-public class StoryDto {
+public class SaveStoryDto {
 
     private Long id;
 
     private String profileUrl;
 
-    private String username;
-
     private String title;
 
     private String content;
 
-    private boolean liked;
+    private String username;
 
-    private int likeCount;
-
-    private int replyCount;
+    private String filename;
 
     private LocalDateTime createdDate;
 
-    public static StoryDto of(Story story, boolean liked, int likeCount, int replyCount) {
-        return StoryDto.builder()
+    private LocalDateTime modifiedDate;
+
+    public static SaveStoryDto of(Story story, Member member) {
+        return SaveStoryDto.builder()
                 .id(story.getId())
-                .profileUrl(story.getMember().getProfileImageUrl())
-                .username(story.getMember().getUsername())
                 .title(story.getTitle())
                 .content(story.getContent())
-                .liked(liked)
-                .likeCount(likeCount)
-                .replyCount(replyCount)
+                .filename(story.getFilePath())
+                .username(member.getUsername())
                 .createdDate(story.getCreatedDate())
+                .modifiedDate(story.getModifiedDate())
                 .build();
     }
 }
--- backend/src/main/java/com/hoppy/app/story/dto/StoryDetailDto.java
+++ backend/src/main/java/com/hoppy/app/story/dto/StoryDetailDto.java
@@ -8,6 +8,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 스토리 리스트에 표기될 단순 정보를 포함한 좋아요 수, 댓글 수를 반환하는 Dto
+ */
+
 @Getter
 @Builder
 @NoArgsConstructor
@@ -15,12 +19,24 @@ import lombok.NoArgsConstructor;
 public class StoryDetailDto {
 
     private Long id;
+
     private String title;
+
     private String content;
+
     private String username;
+
     private String filename;
+
+    private boolean liked;
+
+    private int likeCount;
+
+    private int replyCount;
+
     private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
+
+    private List<StoryReplyDto> replies;
 
     public static StoryDetailDto from(Story story) {
         return StoryDetailDto.builder()
@@ -30,7 +46,6 @@ public class StoryDetailDto {
                 .username(story.getMember().getUsername())
                 .filename(story.getFilePath())
                 .createdDate(story.getCreatedDate())
-                .modifiedDate(story.getModifiedDate())
                 .build();
     }
 }
