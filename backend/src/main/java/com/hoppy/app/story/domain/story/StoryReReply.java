c3e463d7303ad711f37e7dcd5df89a2bd426b5ed
Choidaehan <71701634+Choidaehankr@users.noreply.github.com>
2022-08-22 23:38:38 +0900

    [feat] 스토리 댓글/대댓글 클래스 추가

---
+++ backend/src/main/java/com/hoppy/app/story/domain/story/StoryReReply.java
@@ -0,0 +1,53 @@
+package com.hoppy.app.story.domain.story;
+
+import com.hoppy.app.like.domain.MemberReReplyLike;
+import com.hoppy.app.member.domain.Member;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.ToString.Exclude;
+import org.hibernate.annotations.BatchSize;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class StoryReReply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne
+    @Exclude
+    private StoryReply reply;
+
+    @OneToMany(mappedBy = "reReply", fetch = FetchType.LAZY)
+    @BatchSize(size = 100)
+    @Default
+    @Exclude
+    private Set<MemberReReplyLike> likes = new HashSet<>();
+}
