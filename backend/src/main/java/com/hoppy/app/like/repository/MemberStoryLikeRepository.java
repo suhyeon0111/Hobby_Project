a08c8631b1fe0d6ce708969a6adf7d3687615f17
Daehan Choi <mnnm22@naver.com>
2022-08-25 17:41:03 +0900

    [fix] 스토리 좋아요 레포지토리 @Param 추가

--- backend/src/main/java/com/hoppy/app/like/repository/MemberStoryLikeRepository.java
+++ backend/src/main/java/com/hoppy/app/like/repository/MemberStoryLikeRepository.java
@@ -4,11 +4,15 @@ import com.hoppy.app.like.domain.MemberStoryLike;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberStoryLikeRepository extends JpaRepository<MemberStoryLike, Long> {
 
     @Query("select s from MemberStoryLike s where s.member.id = :memberId and s.story.id = :storyId")
-    Optional<MemberStoryLike> findByMemberIdAndStoryId(Long memberId, Long storyId);
+    Optional<MemberStoryLike> findByMemberIdAndStoryId(
+            @Param("memberId") Long memberId,
+            @Param("storyId") Long storyId
+    );
 }
