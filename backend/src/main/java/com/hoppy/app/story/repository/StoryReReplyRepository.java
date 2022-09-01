bfab17f0c947e5d1b2c149db2c0bb40e71f6e448
Daehan Choi <mnnm22@naver.com>
2022-09-01 17:49:06 +0900

    [fix] 스토리 댓글 및 대댓글 레포지토리 수정

--- backend/src/main/java/com/hoppy/app/story/repository/StoryReReplyRepository.java
+++ backend/src/main/java/com/hoppy/app/story/repository/StoryReReplyRepository.java
@@ -1,5 +1,22 @@
 package com.hoppy.app.story.repository;
 
-public interface StoryReReplyRepository {
+import com.hoppy.app.story.domain.story.StoryReReply;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+public interface StoryReReplyRepository extends JpaRepository<StoryReReply, Long> {
+
+    @Modifying
+    @Query("delete from StoryReReply s where s.id in :idList")
+    void deleteAllByList(@Param("idList") List<Long> idList);
+
+    @Query("select s from StoryReReply s where s.id = :id and s.member.id = :memberId")
+    Optional<StoryReReply> findByIdAndMemberId(
+            @Param("id") Long id,
+            @Param("memberId") Long memberId
+    );
 }
