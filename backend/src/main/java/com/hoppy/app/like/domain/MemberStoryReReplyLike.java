557e77a2229d34995c80fd41ae28f7df19c1c5d1
Daehan Choi <mnnm22@naver.com>
2022-09-01 17:48:29 +0900

    [feat] 스토리 대댓글 추가

---
+++ backend/src/main/java/com/hoppy/app/like/domain/MemberStoryReReplyLike.java
@@ -0,0 +1,52 @@
+package com.hoppy.app.like.domain;
+
+import com.hoppy.app.community.domain.ReReply;
+import com.hoppy.app.member.domain.Member;
+import com.hoppy.app.story.domain.story.StoryReReply;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Table(
+        uniqueConstraints={
+                @UniqueConstraint(
+                        columnNames={"member_id", "reRply_id"}
+                )
+        }
+)
+public class MemberStoryReReplyLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private StoryReReply reReply;
+
+    public static MemberStoryReReplyLike of(Member member, StoryReReply reReply) {
+        return MemberStoryReReplyLike.builder()
+                .member(member)
+                .reReply(reReply)
+                .build();
+    }
+}
