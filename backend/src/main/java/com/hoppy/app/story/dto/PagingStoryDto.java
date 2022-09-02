572f4001fb8ac3d188c6319b28a1073ab2c4e513
Daehan Choi <mnnm22@naver.com>
2022-09-02 16:29:34 +0900

    [hotfix] 스토리 페이지네이션 시 작성자 id 반환

--- backend/src/main/java/com/hoppy/app/story/dto/PagingStoryDto.java
+++ backend/src/main/java/com/hoppy/app/story/dto/PagingStoryDto.java
@@ -15,13 +15,11 @@ import lombok.NoArgsConstructor;
 public class PagingStoryDto {
 
     private List<StoryDto> storyList;
-//    private String nextPagingUrl;
     private String lastId;
 
     public static PagingStoryDto of(List<StoryDto> storyList, String lastId) {
         return PagingStoryDto.builder()
                 .storyList(storyList)
-//                .nextPagingUrl(nextPagingUrl)
                 .lastId(lastId)
                 .build();
     }
