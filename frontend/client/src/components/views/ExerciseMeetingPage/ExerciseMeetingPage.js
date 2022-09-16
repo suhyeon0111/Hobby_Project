ba5e865708f5c17e5565cdb8ac181bc6e28109a1
suhyeon0111 <soo4833@naver.com>
2022-09-17 02:01:24 +0900

    [style]: 카드 리스트 외관 구현

--- frontend/client/src/components/views/ExerciseMeetingPage/ExerciseMeetingPage.js
+++ frontend/client/src/components/views/ExerciseMeetingPage/ExerciseMeetingPage.js
@@ -1,58 +1,99 @@
 import React from "react";
-import {Input} from "antd";
+import { Input } from "antd";
 import Axios from "axios";
-// import CardHandler from '../CardHandler/CardHandler';
+import TestImg from "./TestImg.jpeg";
 
 function ExerciseMeetingPage() {
-    const {Search} = Input;
-    // const {Meta} = Card;
+  const { Search } = Input;
 
-    const onSearch = (value) => console.log(value);
+  const onSearch = (value) => console.log(value);
 
-    Axios.get();
-    return (<div
+  Axios.get();
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        width: "100%",
+      }}
+    >
+      <div
         style={{
+          width: "100%",
+          margin: "3rem auto",
+        }}
+      >
+        <Search
+          placeholder="찾으시는 취미를 검색해보세요!"
+          onSearch={onSearch}
+          style={{
             textAlign: "center",
-            width: "100%"
-        }}>
+            width: "90%",
+          }}
+          // enterButton
+          size="large"
+        />
+        <h3
+          style={{
+            float: "left",
+            paddingTop: "26px",
+            fontSize: "16px",
+            marginLeft: "27px",
+          }}
+        >
+          운동 모임 리스트
+          <span role="img" aria-label="exercise">
+            🏃‍♂️
+          </span>
+        </h3>
+        {/* 모임 리스트 조회 */}
         <div
+          style={{
+            width: "100%",
+          }}
+        >
+          <div
             style={{
-                width: "100%",
-                margin: "3rem auto"
-            }}>
-            <Search placeholder="찾으시는 취미를 검색해보세요!" onSearch={onSearch} style={{
-                    textAlign: "center",
-                    width: "90%"
-                }}
-                // enterButton
-                size="large"/>
-            <h3
-                style={{
-                    float: "left",
-                    paddingTop: "26px",
-                    fontSize: "16px",
-                    marginLeft: "27px"
-                }}>
-                운동 모임 리스트
-                <span role="img" aria-label="exercise">
-                    🏃‍♂️
-                </span>
-            </h3>
-            <div style={{
-                    width: "90%"
-                }}>
-                {/* <Card
-                    hoverable="hoverable"
-                    style={{
-                        width: '170px',
-	@@ -43,10 +54,10 @@ function ExerciseMeetingPage() {
-                    cover={<img alt = "example" src= />}>
-                    <Meta title="Europe Street beat" description="www.instagram.com"/>
-                  </Card>  */
-                }
-                </div>
-        </div>
-    </div>);
-            }
+              width: "90%",
+              margin: "0 auto",
+              // backgroundColor: "#A5A5A5",
+              display: "flex",
+            }}
+          >
+            <div
+              style={{
+                width: "48.5%",
+                height: "200px",
+                border: "0.8px solid #A5A5A5",
+                borderRadius: "8px",
+              }}
+            >
+              <img
+                alt="example"
+                src={TestImg}
+                style={{ width: "90%", marginTop: "9px" }}
+              />
+            </div>
 
-            export default ExerciseMeetingPage;
+            <div
+              style={{
+                width: "48.5%",
+                height: "200px",
+                marginLeft: "3%",
+                border: "0.8px solid #A5A5A5",
+                borderRadius: "8px",
+              }}
+            >
+              <img
+                alt="example"
+                src={TestImg}
+                style={{ width: "90%", marginTop: "9px" }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExerciseMeetingPage;
